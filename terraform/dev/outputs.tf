0ed5d0ea0d6fd92d38a7dc62888da9b0e5412bca
Jia-Ping Chu <jumpingchu@gmail.com>
2025-03-14 09:36:40 +0000

    chore: move module code to main.tf

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,6 +1,9 @@
 output "paw_match_dataset_id" {
-  value = module.bq_tables.paw_match_dataset_id
+  description = "Table ID for each table"
+  value       = google_bigquery_dataset.paw_match.dataset_id
 }
+
 output "adoption_opendata_table_id" {
-  value = module.bq_tables.adoption_opendata_table_id
+  description = "Table ID for each table"
+  value       = google_bigquery_table.adoption_opendata.table_id
 }
