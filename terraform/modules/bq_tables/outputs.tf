59c2f13ff91a68c45930999a22a15866b1aeb628
Jia-Ping Chu <jumpingchu@gmail.com>
2025-03-14 09:32:24 +0000

    refactor: remove module for simpler design

--- terraform/modules/bq_tables/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "paw_match_dataset_id" {
-  description = "Table ID for each table"
-  value       = google_bigquery_dataset.paw_match.dataset_id
-}
-
-output "adoption_opendata_table_id" {
-  description = "Table ID for each table"
-  value       = google_bigquery_table.adoption_opendata.table_id
-}
